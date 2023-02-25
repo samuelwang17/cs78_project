fixed A3C, latest glitch is negative padding in sequence memory, some gradient descent has been observed

model_components.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 class grad_skip_softmax(nn.Module):
     def __init__(self) -> None:
         super().__init__()
-        self.sm = nn.Softmax()
+        self.sm = nn.Softmax(dim=1)
     
     def forward(self, x):
         return self.sm(x)