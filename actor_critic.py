8a29fbd01cb7f114eb48ee093060cd5c3a954375
vincentyliu2001 <yishiaoliu@gmail.com>
2023-02-28 00:20:41 -0500

    parallel fix

--- actor_critic.py
+++ actor_critic.py
@@ -9,36 +9,38 @@ from itertools import chain
 import numpy as np
 from model_components import grad_skip_softmax, grad_skip_logsoftmax
 import time
+
+
 class Agent(nn.Module):
     def __init__(self,
-        model_dim,
-        mlp_dim,
-        attn_heads,
-        sequence_length,
-        enc_layers,
-        memory_layers,
-        mem_length,
-        dec_layers,
-        action_dim,
-    ) -> None:
+                 model_dim,
+                 mlp_dim,
+                 attn_heads,
+                 sequence_length,
+                 enc_layers,
+                 memory_layers,
+                 mem_length,
+                 dec_layers,
+                 action_dim,
+                 ) -> None:
         super().__init__()
         self.model = RLformer(
-            model_dim = model_dim,
-            mlp_dim = mlp_dim,
-            attn_heads = attn_heads,
-            sequence_length = sequence_length,
-            enc_layers = enc_layers,
+            model_dim=model_dim,
+            mlp_dim=mlp_dim,
+            attn_heads=attn_heads,
+            sequence_length=sequence_length,
+            enc_layers=enc_layers,
             memory_layers=memory_layers,
-            mem_length = mem_length,
-            dec_layers = dec_layers,
-            action_dim = action_dim,
+            mem_length=mem_length,
+            dec_layers=dec_layers,
+            action_dim=action_dim,
         )
-        self.tokenizer =  Tokenizer(model_dim=model_dim)
+        self.tokenizer = Tokenizer(model_dim=model_dim)
         self.hand_dict = {}
 
     def init_player(self, player, hand):
         # initialize this players hand and tokenize it, store it in buffer
-        hand_tensor = self.tokenizer(hand) # hand needs to be card observations -- list of length two of tensors
+        hand_tensor = self.tokenizer(hand)  # hand needs to be card observations -- list of length two of tensors
         assert hand_tensor != None
         self.hand_dict[player] = hand_tensor
 
@@ -46,54 +48,57 @@ class Agent(nn.Module):
         # takes flattened inputs in list form, not tokenized
         enc_input = []
         dec_input = []
-        for x in range(len(obs_flat)): # expects obs_flat to be a list of flattened observation lists
-            dec_input.append(self.tokenizer(obs_flat[x])) # env steam is what obs_flat used to be
+        for x in range(len(obs_flat)):  # expects obs_flat to be a list of flattened observation lists
+            dec_input.append(self.tokenizer(obs_flat[x]))  # env steam is what obs_flat used to be
             enc_input.append(self.hand_dict[player[x]])
-        policy_logits, value = self.model(enc_input, dec_input, player, new_hand) # expects a list of tensors for dec_input
+        policy_logits, value = self.model(enc_input, dec_input, player,
+                                          new_hand)  # expects a list of tensors for dec_input
         return policy_logits, value
 
+
 class actor_critic():
-    #Needs to be able to run hand, return loss with grad enabled
-    def __init__(self, 
-    model_dim,
-    mlp_dim,
-    heads,
-    enc_layers,
-    memory_layers,
-    mem_length,
-    dec_layers,
-    max_sequence: int = 200, 
-    n_players: int = 2,
-    gamma: float = .8,
-    n_actions: int = 10, # random placeholder value
-    ) -> None:
-    
+    # Needs to be able to run hand, return loss with grad enabled
+    def __init__(self,
+                 model_dim,
+                 mlp_dim,
+                 heads,
+                 enc_layers,
+                 memory_layers,
+                 mem_length,
+                 dec_layers,
+                 max_sequence: int = 200,
+                 n_players: int = 2,
+                 gamma: float = .8,
+                 n_actions: int = 10,  # random placeholder value
+                 ) -> None:
+
         self.gamma = gamma
-        self.env = poker_env(n_players = n_players, batch_size = 128)
+        self.env = poker_env(n_players=n_players, batch_size=128)
         self.agent = Agent(
-            model_dim = model_dim,
-            mlp_dim = mlp_dim,
-            attn_heads = heads,
-            sequence_length = max_sequence,
-            enc_layers = enc_layers,
-            memory_layers= memory_layers,
+            model_dim=model_dim,
+            mlp_dim=mlp_dim,
+            attn_heads=heads,
+            sequence_length=max_sequence,
+            enc_layers=enc_layers,
+            memory_layers=memory_layers,
             mem_length=mem_length,
-            dec_layers = dec_layers,
-            action_dim = n_actions,
+            dec_layers=dec_layers,
+            action_dim=n_actions,
         )
         self.n_players = n_players
 
-        self.observations = [] #this will be a list of lists, each is the list of observations in a hand
-        self.obs_flat = list(chain(*self.observations))
-        
-        self.rewards = []
-        self.rewards_flat = list(chain(*self.rewards))
+        self.observations = [[] for _ in
+                             range(128)]  # this will be a list of lists, each is the list of observations in a hand
+        self.obs_flat = [[] for _ in range(128)]
 
-        self.values = []
-        self.val_flat = list(chain(*self.values))
+        self.rewards = [[] for _ in range(128)]
+        self.rewards_flat = [[] for _ in range(128)]
 
-        self.action_log_probabilies = []
-        self.alp_flat = list(chain(*self.action_log_probabilies))
+        self.values = [[] for _ in range(128)]
+        self.val_flat = [[] for _ in range(128)]
+
+        self.action_log_probabilies = [[] for _ in range(128)]
+        self.alp_flat = [[] for _ in range(128)]
 
         self.max_sequence = max_sequence
 
@@ -104,8 +109,8 @@ class actor_critic():
         self.softmax = grad_skip_softmax()
 
         self.padding = nn.ConstantPad1d(
-            padding = n_players - 1,
-            value = -100000
+            padding=n_players - 1,
+            value=-100000
         )
 
         self.lsm = grad_skip_logsoftmax()
@@ -113,7 +118,6 @@ class actor_critic():
         self.silu = nn.SiLU()
 
         self.time_dict = {'total': 0, 'env': 0, 'model_inference': 0, 'loss': 0}
-
 
     def sample_action(self, curr_logits):
         # MASK, SAMPLE, DETOKENIZE
@@ -123,28 +127,28 @@ class actor_critic():
         pot = self.env.pot
         linspace_dim = self.n_actions - 4
         assert linspace_dim > 0
-        
+
         # MASK
         mask = [0] * self.n_actions
         if self.env.behind[player] != 0:
-            mask[3] = 1 # cannot check
+            mask[3] = 1  # cannot check
         else:
-            mask[1] = 1 # cannot call
-            mask[2] = 1 # cannot fold if not facing a bet
+            mask[1] = 1  # cannot call
+            mask[2] = 1  # cannot fold if not facing a bet
 
         if self.env.behind[player] == self.env.stacks[player]:
             mask[0] = 1  # cannot shove if shoving and calling are equivalent
-        
-        linspace = np.geomspace(.5, 2, num = linspace_dim)
+
+        linspace = np.geomspace(.5, 2, num=linspace_dim)
         stack_checker = lambda x: 1 if x * pot >= player_stack or x < (2 * self.env.current_largest_bet) else 0
-        mask[4:] += np.fromiter((stack_checker(x) for x in linspace), linspace.dtype) # mask away bets that are larger than stack or all in or bets that are are not 2x larger than last bet
+        mask[4:] += np.fromiter((stack_checker(x) for x in linspace),
+                                linspace.dtype)  # mask away bets that are larger than stack or all in or bets that are are not 2x larger than last bet
         tensor_mask = torch.Tensor(mask)
 
         # grad skip softmax -- neural replicator dynamics, with mask
         policy = self.softmax(curr_logits.masked_fill(tensor_mask == 1, float('-inf')))
 
         np_dist = np.squeeze(policy[-1].detach().numpy())
-        
 
         # SAMPLE
         action_index = np.random.choice(self.n_actions, p=np_dist)
@@ -154,129 +158,130 @@ class actor_critic():
         alp = alps[action_index]
 
         # DETOKENIZE
-        if action_index == 0: # all in
+        if action_index == 0:  # all in
             action = {'player': player, 'type': 'bet', 'value': player_stack}
-        elif action_index == 1: # call
+        elif action_index == 1:  # call
             action = {'player': player, 'type': 'call', 'value': 0}
-        elif action_index == 2: # fold
+        elif action_index == 2:  # fold
             action = {'player': player, 'type': 'fold', 'value': 0}
-        elif action_index == 3: # check
+        elif action_index == 3:  # check
             action = {'player': player, 'type': 'bet', 'value': 0}
         else:
             action = {'player': player, 'type': 'bet', 'value': (linspace[action_index - 4] * pot) // 1}
 
         return alp, action
-    
+
     def init_hands(self):
         # get all hands
         # run encoder for each of players
         for player in range(self.n_players):
             hand = self.env.get_hand(player)
             self.agent.init_player(player, hand)
-    
+
     def chop_seq(self):
-        #if length of observations is above a certain size, chop it back down to under sequence length by removing oldest hand
-        #return flattened version to give to model on next run
-        self.obs_flat = list(chain(*self.observations))
-        if len(self.obs_flat) > self.max_sequence:
-            before = len(self.obs_flat)
-            self.observations = self.observations[1:]
-            self.obs_flat = list(chain(*self.observations))
-            after = len(self.obs_flat)
-            assert len(self.obs_flat) <= self.max_sequence, f'prechop: {before}, postchop: {after}'
+        # if length of observations is above a certain size, chop it back down to under sequence length by removing oldest hand
+        # return flattened version to give to model on next run
+        self.obs_flat = []
+        for observation in self.observations:
+            self.obs_flat.append(list(chain(*observation)))
+        for i in range(len(self.observations)):
+            if len(self.obs_flat[i]) > self.max_sequence:
+                before = len(self.obs_flat[i])
+                self.observations[i] = self.observations[i][1:]
+                self.obs_flat[i] = list(chain(*self.observations[i]))
+                after = len(self.obs_flat[i])
+                assert len(self.obs_flat[i]) <= self.max_sequence, f'prechop: {before}, postchop: {after}'
 
-            self.rewards = self.rewards[1:]
-            self.rewards_flat = list(chain(*self.rewards))
-            
+                self.rewards[i] = self.rewards[i][1:]
+                self.rewards_flat[i] = list(chain(*self.rewards[i]))
 
-            self.values = self.values[1:]
-            self.val_flat = list(chain(*self.values))
+                self.values[i] = self.values[i][1:]
+                self.val_flat[i] = list(chain(*self.values[i]))
 
-            self.action_log_probabilies = self.action_log_probabilies[1:]
-            self.alp_flat = list(chain(*self.action_log_probabilies))
-            
+                self.action_log_probabilies[i] = self.action_log_probabilies[i][1:]
+                self.alp_flat[i] = list(chain(*self.action_log_probabilies[i]))
 
-        else:
 
-            self.obs_flat = list(chain(*self.observations))
-            self.rewards_flat = list(chain(*self.rewards))
-            self.val_flat = list(chain(*self.values))
-            self.alp_flat = list(chain(*self.action_log_probabilies))
-        
-        assert len(self.obs_flat) <= self.max_sequence
+            else:
+                self.rewards_flat[i] = list(chain(*self.rewards[i]))
+                self.val_flat[i] = list(chain(*self.values[i]))
+                self.alp_flat[i] = list(chain(*self.action_log_probabilies[i]))
 
+        assert len(self.obs_flat[i]) <= self.max_sequence
 
     def play_hand(self):
         self.time_dict = {'total': 0, 'env': 0, 'model_inference': 0, 'loss': 0}
         clock1 = time.time_ns()
         # makes agent play one hand
         # deal cards
-        rewards, observations = self.env.new_hand() # start a new hand, observations is list of observation lists
-        self.init_hands() # pre load all of the hands
+        rewards, observations = self.env.new_hand()  # start a new hand, observations is list of observation lists
+        self.init_hands()  # pre load all of the hands
         # init lists for this hand
         for x in range(len(self.observations)):
             self.observations[x].append(observations[x])
         for x in range(len(self.rewards)):
             self.rewards[x].append(rewards[x])
-        self.chop_seq() # prepare for input to model
+        self.chop_seq()  # prepare for input to model
         self.values.append([])
         self.action_log_probabilies.append([])
         for x in range(len(self.values[0])):
-                new_values = [-5783] * self.n_players # -5783 filler value
-                for x in range(len(self.values)):
-                    self.values[x][-1].append(torch.Tensor(new_values))
-                new_alps = [0] * self.n_players
-                for x in range(len(self.action_log_probabilies)):
-                    self.action_log_probabilies[x][-1].append(torch.Tensor(new_alps))
-        
+            new_values = [-5783] * self.n_players  # -5783 filler value
+            for x in range(len(self.values)):
+                self.values[x][-1].append(torch.Tensor(new_values))
+            new_alps = [0] * self.n_players
+            for x in range(len(self.action_log_probabilies)):
+                self.action_log_probabilies[x][-1].append(torch.Tensor(new_alps))
+
         hand_over = False
         while not hand_over:
             # get values and policy -- should be in list form over sequence length
             player = self.env.in_turn
-            
-            clock = time.time_ns()
-            policy_logits, values = self.agent(player, self.obs_flat, new_hand = True)
-            assert False, player
-            value = values.squeeze()[-1] # get last value estimate
-            assert value.requires_grad
-            curr_logits = policy_logits[-1] # get last policy distribution
 
-            
-            y_logit, action = self.sample_action(curr_logits) # handles mask, softmax, sample, detokenization
-            
-            rewards, obs, hand_over = self.env.take_action(action) # need to change environment to return hand_over boolean
-            
+            clock = time.time_ns()
+            policy_logits, values = self.agent(player, self.obs_flat, new_hand=True)
+            assert False, player
+            value = values.squeeze()[-1]  # get last value estimate
+            assert value.requires_grad
+            curr_logits = policy_logits[-1]  # get last policy distribution
+
+            alp, action = self.sample_action(curr_logits)  # handles mask, softmax, sample, detokenization
+
+            rewards, obs, hand_over = self.env.take_action(
+                action)  # need to change environment to return hand_over boolean
+
             # add new information from this step
-            self.rewards[-1] += rewards #add tensor
+            self.rewards[-1] += rewards  # add tensor
             self.observations[-1] += obs
 
             # value needs to be on a per player basis
 
-            new_values = self.padding(value.unsqueeze(-1))[(self.n_players - player - 1):(2 * self.n_players - player - 1)].squeeze()
+            new_values = self.padding(value.unsqueeze(-1))[
+                         (self.n_players - player - 1):(2 * self.n_players - player - 1)].squeeze()
             self.values[-1].append(new_values)
             for x in range(len(rewards) - 1):
-                new_values = [-100000] * self.n_players #-10000 is filler value
+                new_values = [-100000] * self.n_players  # -10000 is filler value
                 # fill_tensor = torch.Tensor(new_values)
                 self.values[-1].append(torch.Tensor(new_values))
-            
-            new_alp = self.padding(y_logit.unsqueeze(-1))[(self.n_players - player - 1):(2 * self.n_players - player - 1)].squeeze()
+
+            new_alp = self.padding(alp.unsqueeze(-1))[
+                      (self.n_players - player - 1):(2 * self.n_players - player - 1)].squeeze()
             self.action_log_probabilies[-1].append(new_alp)
             for x in range(len(rewards) - 1):
                 new_alps = [-100000] * self.n_players
                 self.action_log_probabilies[-1].append(torch.Tensor(new_alps))
-           
+
             # prepare for next action
             self.chop_seq()
 
         clock = time.time_ns()
         Vals_T = [0] * self.n_players
         for player in range(self.n_players):
-            _, V_T = self.agent(player, self.obs_flat, new_hand = False)
+            _, V_T = self.agent(player, self.obs_flat, new_hand=False)
             Vals_T[player] = V_T.squeeze()[-1]
         self.time_dict['model_inference'] += time.time_ns() - clock
         # process gradients and return loss:
         out = self.get_loss(torch.stack(Vals_T))
-        self.time_dict['total'] += time.time_ns() - clock1
+        self.time_dict['total'] = time.time_ns() - clock1
         return out
 
     def get_loss(self, Vals_T):
@@ -284,29 +289,27 @@ class actor_critic():
         Qs = [0] * len(self.rewards[-1])
         Q_t = Vals_T
         for t in reversed(range(len(self.rewards[-1]))):
-            Q_t = self.rewards[-1][t] + self.gamma * Q_t #adds rewards up going backwards to get vals
+            Q_t = self.rewards[-1][t] + self.gamma * Q_t  # adds rewards up going backwards to get vals
             Qs[t] = Q_t
-        
 
-        Qs = torch.stack(Qs)[1:] #2d tensor sequence, players
+        Qs = torch.stack(Qs)[1:]  # 2d tensor sequence, players
 
         values = torch.stack(self.values[-1])[:-1]
 
         # set Qs to filler value where value is filler value
         Qs = Qs.masked_fill(values == -100000, -100000)
         alps = torch.stack(self.action_log_probabilies[-1])[:-1]
-        advantages = Qs - values 
-        advantages = advantages.masked_fill(values == -5783, 0) # using arbitrary filler from earlier to mask out the blinds
-        
-        actor_loss = (-alps * advantages).mean() # loss function for policy going into softmax on backpass
-        critic_loss = (advantages).pow(2).mean() / 200 # autogressive critic loss - MSE
-        
+        advantages = Qs - values
+        advantages = advantages.masked_fill(values == -5783,
+                                            0)  # using arbitrary filler from earlier to mask out the blinds
+
+        actor_loss = (-alps * advantages).mean()  # loss function for policy going into softmax on backpass
+        critic_loss = (advantages).pow(2).mean() / 200  # autogressive critic loss - MSE
+
         loss = actor_loss + critic_loss
         self.time_dict['loss'] = time.time_ns() - clock
         return loss, actor_loss, critic_loss, self.time_dict
-    
 
     def clear_memory(self):
         # zero the gradients of the model
         self.agent.model.zero_grad()
-    
--- model_components.py
+++ model_components.py
@@ -4,54 +4,56 @@
 import torch
 import torch.nn as nn
 
+
 class grad_skip_softmax(nn.Module):
     def __init__(self) -> None:
         super().__init__()
         self.sm = nn.Softmax(dim=1)
-    
+
     def forward(self, x):
         return self.sm(x)
 
     def backward(self, grad):
         # skip gradient through the softmax on backward pass
         return grad
-    
+
+
 class grad_skip_logsoftmax(nn.Module):
     def __init__(self) -> None:
         super().__init__()
         self.lsm = nn.LogSoftmax(dim=1)
-    
+
     def forward(self, x):
         return self.lsm(x)
-    
+
     def backward(self, grad):
-        return 1/grad
-        
+        return 1 / grad
+
+
 class gru(nn.Module):
     # 'gated-recurrent-unit type gating' as seen in GTrXL paper
-    def __init__(self, dim, b_g = 1) -> None:
+    def __init__(self, dim, b_g=1) -> None:
         super().__init__()
 
-        self.w_r = nn.Linear(dim, dim, bias = False)
-        self.u_r = nn.Linear(dim, dim, bias = False)
+        self.w_r = nn.Linear(dim, dim, bias=False)
+        self.u_r = nn.Linear(dim, dim, bias=False)
 
-        self.w_z = nn.Linear(dim, dim, bias = True)
-        self.u_z = nn.Linear(dim, dim, bias = True)
-        self.b_g = b_g # this is used to hack initial bias of the above to be below zero, such that gate is initialized close to identity
-        self.w_g = nn.Linear(dim, dim, bias = False)
-        self.u_g = nn.Linear(dim, dim, bias = False)
+        self.w_z = nn.Linear(dim, dim, bias=True)
+        self.u_z = nn.Linear(dim, dim, bias=True)
+        self.b_g = b_g  # this is used to hack initial bias of the above to be below zero, such that gate is initialized close to identity
+        self.w_g = nn.Linear(dim, dim, bias=False)
+        self.u_g = nn.Linear(dim, dim, bias=False)
 
         self.sigmoid = nn.Sigmoid()
         self.tanh = nn.Tanh()
 
     def forward(self, x, y):
-
         r = self.sigmoid(self.w_r(y) + self.u_r(x))
-        z = self.sigmoid(self.w_z(y) + self.u_z(x) - self.b_g) # when zero, gate passes identity of residual
+        z = self.sigmoid(self.w_z(y) + self.u_z(x) - self.b_g)  # when zero, gate passes identity of residual
         h_hat = self.tanh(self.w_g(y) + self.u_g(r * x))
-        g = (1-z)*x + z * h_hat
+        g = (1 - z) * x + z * h_hat
         return g
-        
+
 
 class mlp(nn.Module):
     # 1d temporal convolution
@@ -63,7 +65,7 @@ class mlp(nn.Module):
             nn.ReLU(),
             nn.Dropout(.1),
             nn.Linear(internal_dim, embed_dim)
-        ) # no second relu at output of mlp
+        )  # no second relu at output of mlp
 
     def forward(self, input):
         return self.block(input)
@@ -75,11 +77,14 @@ class cross_attention(nn.Module):
         self.attention = nn.MultiheadAttention(
             embed_dim=embed_dimension,
             num_heads=num_heads,
-            )
-    
+            batch_first=True
+        )
+
     def forward(self, x, enc):
         x = x.squeeze()
+        print(x.size())
         return self.attention(x, enc, enc)[0]
+
 
 class self_attention(nn.Module):
     def __init__(self, embed_dimension, num_heads) -> None:
@@ -87,35 +92,34 @@ class self_attention(nn.Module):
         self.attention = nn.MultiheadAttention(
             embed_dim=embed_dimension,
             num_heads=num_heads,
-            )
-    
+        )
+
     def forward(self, x):
-
         return self.attention(x, x, x)[0]
-
-
 
 
 class Smear_key(nn.Module):
 
     def __init__(self,
-    sequence_length,
-    heads
-    ) -> None:
+                 sequence_length,
+                 heads
+                 ) -> None:
         super().__init__()
         self.alpha = nn.Parameter(torch.ones(1, heads, sequence_length - 1, 1))
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, k):
         itrp = self.sigmoid(self.alpha)
-        smear = k[:,:,1:,:]*itrp + k[:,:,:-1,:]*(1-itrp)
-        return torch.cat([k[:,:, 0:1, :], smear], dim = 2)
+        smear = k[:, :, 1:, :] * itrp + k[:, :, :-1, :] * (1 - itrp)
+        return torch.cat([k[:, :, 0:1, :], smear], dim=2)
+
 
 class decoder_mha(nn.Module):
-    #Masked smeared self attention
+    # Masked smeared self attention
     def __init__(self, model_dim, sequence_length, heads) -> None:
         super().__init__()
-        self.mask = torch.triu(torch.ones(sequence_length, sequence_length) * float('-inf'), diagonal=1) # make batch, heads, seq,seq
+        self.mask = torch.triu(torch.ones(sequence_length, sequence_length) * float('-inf'),
+                               diagonal=1)  # make batch, heads, seq,seq
         self.model_dim = model_dim
         self.sequence_length = sequence_length
         self.heads = heads
@@ -127,52 +131,21 @@ class decoder_mha(nn.Module):
         self.ln = nn.LayerNorm(model_dim)
         self.smear = Smear_key(sequence_length, heads)
 
-    def forward(self,x):
+    def forward(self, x):
         # batch, sequence, model_dim
-        q = self.W_q(x).view(-1, self.sequence_length, self.heads, self.key_dim).transpose(1,2)
-        k = self.W_k(x).view(-1, self.sequence_length, self.heads, self.key_dim).transpose(1,2)
-        v = self.W_v(x).view(-1, self.sequence_length, self.heads, self.key_dim).transpose(1,2)
+        q = self.W_q(x).view(-1, self.sequence_length, self.heads, self.key_dim).transpose(1, 2)
+        k = self.W_k(x).view(-1, self.sequence_length, self.heads, self.key_dim).transpose(1, 2)
+        v = self.W_v(x).view(-1, self.sequence_length, self.heads, self.key_dim).transpose(1, 2)
         k = self.smear(k)
         # batch, heads, sequence, dim // heads
         key_dim = k.shape[-1:][0]
-        scores = q @ k.transpose(2,3) / key_dim**.5
+        scores = q @ k.transpose(2, 3) / key_dim ** .5
         scores += self.mask
-        attn = torch.softmax(scores, dim = 3)
+        attn = torch.softmax(scores, dim=3)
         mha = attn @ v
         mha = mha.transpose(1, 2).contiguous().view(-1, self.sequence_length, self.model_dim)
         out = self.output(mha)
         # batch, sequence, model_dim
         return out
-
-class critic_head(nn.Module):
-    
-    def __init__(self,
-        value_points,
-        model_dim,
-        critic_dim
-        ) -> None:
-        super().__init__()
-
-        self.value_outs = nn.Sequential(
-            nn.Linear(model_dim,critic_dim),
-            nn.ReLU(),
-            nn.Linear(critic_dim, value_points),
-            nn.ReLU()
-        )
-
-        self.gating = nn.Sequential(
-            nn.Linear(model_dim, critic_dim),
-            nn.ReLU(),
-            nn.Linear(critic_dim, value_points),
-            nn.LayerNorm(value_points),
-            grad_skip_softmax()
-        )
-
-    def forward(self, x):
-        values = self.value_outs(x)
-        gate = self.gating(x)
-        weighted_values = values * gate
-        return weighted_values.sum()
-        
 
 
