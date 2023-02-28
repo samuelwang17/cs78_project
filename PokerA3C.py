c8e0704bcafd5ef2481d54625f72e816e686daa6
vincentyliu2001 <yishiaoliu@gmail.com>
2023-02-28 14:38:34 -0500

    parallel fix

--- PokerA3C.py
+++ PokerA3C.py
@@ -7,7 +7,9 @@ from pokerenv import poker_env
 from actor_critic import *
 import torch.multiprocessing as mp
 from shared_adam import SharedAdam
-from transformer import *
+from transformer import mod_transformer as RLformer
+import warnings
+
 
 'inspired from https://github.com/MorvanZhou/pytorch-A3C/blob/master/discrete_A3C.py'
 
@@ -59,29 +61,30 @@ class Player(mp.Process):
                 self.global_actor_ema.value = self.global_actor_ema.value * .995 + actor_loss * .005 if self.global_actor_ema.value != 0 else actor_loss
             with self.global_critic_ema.get_lock():
                 self.global_critic_ema.value = self.global_critic_ema.value * .995 + critic_loss * .005 if self.global_critic_ema.value != 0 else critic_loss
-            if self.episode_idx.value % 100 == 0:
+            if self.episode_idx.value % 1 == 0:
                 print(f'episode: {self.episode_idx.value}, loss: {self.global_loss_ema.value * 100 // 1 / 100}, actor loss: {self.global_actor_ema.value * 100 // 1 / 100}, critic_loss: {self.global_critic_ema.value * 100 // 1}')
                 print(f'total time: {time_dict["total"]}, model: {time_dict["model_inference"]/time_dict["total"]}')
 
 
 if __name__ == '__main__':
+    warnings.filterwarnings("ignore")
     torch.manual_seed(0)
     N_GAMES = 100
-    actor_count = 4
+    actor_count = 3
     # actor parameters
     max_sequence = 200
     n_players = 2
     gamma = 1
     n_actions = 6
     # model parameters
-    model_dim = 64
-    mlp_dim = 128
+    model_dim = 32
+    mlp_dim = 64
     attn_heads = 8
     sequence_length = 50
-    enc_layers = 3
+    enc_layers = 4
     memory_layers = 0 #pre_mem, mem layered
     mem_length = 25
-    dec_layers = 4
+    dec_layers = 8
     action_dim = 8
     learning_rate = .0001
     player_params = [model_dim, mlp_dim, attn_heads, enc_layers, memory_layers, mem_length, dec_layers, sequence_length, n_players, learning_rate, action_dim]
@@ -108,7 +111,6 @@ if __name__ == '__main__':
     [player.start() for player in players]
     [player.join() for player in players]
     torch.save(global_model, 'PokerA3CModel_new')
-
 
 
 
--- transformer.py
+++ transformer.py
@@ -6,20 +6,21 @@ from encoder import encoder
 from decoder import decoder
 from model_components import critic_head
 
+
 class mod_transformer(nn.Module):
     # tested and functional
 
     def __init__(self,
-    model_dim,
-    mlp_dim,
-    attn_heads,
-    sequence_length,
-    enc_layers,
-    memory_layers,
-    mem_length,
-    dec_layers,
-    action_dim,
-    ) -> None:
+                 model_dim,
+                 mlp_dim,
+                 attn_heads,
+                 sequence_length,
+                 enc_layers,
+                 memory_layers,
+                 mem_length,
+                 dec_layers,
+                 action_dim,
+                 ) -> None:
         super().__init__()
 
         self.encoder = encoder(
@@ -31,7 +32,7 @@ class mod_transformer(nn.Module):
 
         self.decoder = decoder(
             layers=dec_layers,
-            model_dim= model_dim,
+            model_dim=model_dim,
             mlp_dim=mlp_dim,
             heads=attn_heads,
             sequence_length=sequence_length,
@@ -43,37 +44,32 @@ class mod_transformer(nn.Module):
             nn.Linear(model_dim, model_dim),
             nn.ReLU(),
             nn.Linear(model_dim, action_dim)
-        ) # actor returns logits, softmax handled at higher level
+        )  # actor returns logits, softmax handled at higher level
 
-        self.critic = nn.Sequential(
-            nn.Linear(model_dim, model_dim),
-            nn.ReLU(),
-            nn.Linear(model_dim, 1)
+        self.critic = critic_head(
+            value_points=5,
+            model_dim=model_dim,
+            critic_dim=model_dim * 2
         )
-        # critic_head(
-        #     value_points=5,
-        #     model_dim=model_dim,
-        #     critic_dim=model_dim*2
-        # )
-        
+
         self.seen = {0: False, 1: False}
 
-    def forward(self, enc_input, dec_input, player, new_hand):
+    def forward(self, enc_input, dec_input, player, new_hand, games_per_run):
         if new_hand:
             self.seen = [{0: False, 1: False}] * len(dec_input)
             self.hands = [{}] * len(dec_input)
-        
-        enc = [[] for _ in range(128)]
+
+        enc = [[] for _ in range(games_per_run)]
         for x in range(len(dec_input)):
             if self.seen[x][player[x]]:
                 enc[x] = self.hands[x][player[x]]
-        
+
             else:
                 self.seen[x][player[x]] = True
                 enc[x] = self.encoder(enc_input[x])
                 self.hands[x][player[x]] = enc[x]
         enc = torch.stack(enc)
-        dec = self.decoder(dec_input, enc) #expects a list of tensors as dec_input, enc_input
+        dec = self.decoder(dec_input, enc)  # expects a list of tensors as dec_input, enc_input
         policy_logits = self.actor(dec)
         value = self.critic(dec)
         return policy_logits, value
