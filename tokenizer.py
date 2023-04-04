460a46588254d3330a482e70235c7418b70d60e7
1500banks <1500banks@gmail.com>
2023-04-04 17:05:33 -0400

    updated tokenizer, need to add stack awareness to observations now

--- tokenizer.py
+++ tokenizer.py
@@ -62,14 +62,79 @@ def tokenize(observation):
 
 class Tokenizer(nn.Module):
 
-    def __init__(self, model_dim) -> None:
+    def __init__(self, n_players, token_dim_dict) -> None:
         super().__init__()
-        self.embedding = nn.Sequential(
-            nn.Linear(36, 2*model_dim), # tokenizer has 36 dimensional output
-            nn.ReLU(), # allows feature superposition in embedding
-            nn.LayerNorm(2*model_dim),
-            nn.Linear(2*model_dim, model_dim)
-        )
+        # pos, action, pot, stack
+        # card
+        assert token_dim_dict['pot_edim'] + token_dim_dict['action_edim'] + token_dim_dict['pos_edim'] + token_dim_dict['stack_edim'] == token_dim_dict['card_edim']
+        
+        # embeddings that take value as argument:
+        self.bet_embedding = nn.Sequential(nn.Linear(1, token_dim_dict['action_edim']), nn.ReLU(), nn.Linear(token_dim_dict['action_edim'], token_dim_dict['action_edim']), nn.ReLU())
+
+        self.pot_embedding = nn.Sequential(nn.Linear(1, token_dim_dict['pot_edim']), nn.ReLU(), nn.Linear(token_dim_dict['pot_edim'], token_dim_dict['pot_edim']), nn.ReLU())
+
+        # embeddings that require no arguments
+        self.card_embedding = nn.Embedding(num_embeddings = 53, embedding_dim = token_dim_dict['card_edim'])
+        self.player_embedding = nn.Embedding(num_embeddings = n_players, embedding_dim = token_dim_dict['pos_edim']) # done by blinds ordering
+        self.action_embedding = nn.Embedding(num_embeddings= 3, embedding_dim = token_dim_dict['action_edim']) # call, fold, win
+        self.stack_emdedding = nn.Embedding(num_embeddings=401, embedding_dim= token_dim_dict['stack_edim']) # player's stack can be in range 0-400 inclusive
+    
+    def tokenize(self, observation):
+        '''
+        for actions:
+        all that matters is money into the pot -- everyone else will have to match this amount of money. 
+        
+        only other option is fold - this should get own token
+        
+        other options are wins and cards. cards already handled
+        wins should get own token and value embedding
+
+        so 4 value embeddings - one for money into pot, one for winning money, one for stack size, one for pot size
+        then, 
+
+
+        '''
+        
+        if observation['type'] == 'card':
+            # complete
+            suit = observation['suit']
+            if suit == 'h':
+                suit_idx = 0
+            elif suit == 'd':
+                suit_idx = 1
+            elif suit == 's':
+                suit_idx = 2
+            elif suit == 'c':
+                suit_idx = 3
+
+            card_idx = suit_idx * 4 + (observation['rank'] - 2) # rank of card (2 through ace (14))
+        
+            return self.card_embedding(card_idx)
+
+        else:
+            # value dependent embeddings, player specific
+            table_pos_embedded_tens = self.player_embedding(observation['player'])
+            player_stack_embedded_tens = self.stack_emdedding(observation['stack'])
+            pot_embedded_tens = self.pot_embedding(observation['pot'])
+            
+            # win
+            if observation['type'] == 'win':
+                action_embedded_tens = self.action_embedding[0]
+            # call
+            elif observation['type'] == 'call':
+                action_embedded_tens = self.action_embedding[1]
+
+            # fold
+            elif observation['type'] == 'fold':
+                action_embedded_tens = self.action_embedding[2]
+
+            # bet/check
+            else:
+                action_embedded_tens = self.bet_embedding(observation['value'])
+            
+            # concat embeddings and return
+            return torch.cat([table_pos_embedded_tens, action_embedded_tens, pot_embedded_tens, player_stack_embedded_tens]) # player, bet size, pot size
+    
 
     def tokenize_list(self, observations):
         # convert list of observations to 2d tensor
@@ -85,3 +150,4 @@ class Tokenizer(nn.Module):
         obs_tensor = self.tokenize_list(observations)
         token = self.embedding(obs_tensor) # sequence, model_dim
         return token
+    
